4811b79cc7c8def0fbeafd4139323296655aa52f
lilop-ua <mag-ua@ukr.net>
2019-02-14 15:13:58 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -6,12 +6,12 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     return 'Hello world!'
 
-dag = DAG('hello_world', description='Simple tutorial DAG',
+dag = DAG('hello_world2', description='Simple tutorial DAG',
           schedule_interval='0/1 * * * *',
           start_date=datetime(2019, 1, 1), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+dummy_operator = DummyOperator(task_id='dummy_task2', retries=3, dag=dag)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(task_id='hello_task2', python_callable=print_hello, dag=dag)
 
 dummy_operator >> hello_operator
